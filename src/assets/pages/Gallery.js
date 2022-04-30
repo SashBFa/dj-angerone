c2a314f4e7f201661392e9e002602b847a17fe46
SashBFa <bastienfajs@gmail.com>
2022-04-30 15:11:11 +0200

    mobile gallery page v1

--- src/assets/pages/Gallery.js
+++ src/assets/pages/Gallery.js
@@ -2,12 +2,24 @@ import React from "react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Navigation from "../components/Navigation";
+import { galleryData } from "./../data/gallery";
 
 const Gallery = () => {
   return (
     <div className="gallery">
       <Navigation />
       <Header page={"gallery"} />
+      <div className="gallery__display" style={{ marginBottom: "4rem" }}>
+        {galleryData &&
+          galleryData.map((pict) => (
+            <img
+              key={pict.id}
+              src={pict.img}
+              alt="galleryImg"
+              style={{ width: "100%", marginTop: "2rem" }}
+            />
+          ))}
+      </div>
       <Footer />
     </div>
   );
