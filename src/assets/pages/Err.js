fa4f762f9a49ba9eb0c5c526abf8de3c996833d1
SashBFa <bastienfajs@gmail.com>
2022-04-30 09:58:10 +0200

    mobile error page v1

--- src/assets/pages/Err.js
+++ src/assets/pages/Err.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Err = () => {
   return (
-    <div>
-      <h1>Error 404</h1>
+    <div className="error">
+      <h1>Oops!</h1>
+      <h2>404 - PAGE NOT FOUND</h2>
+      <p>
+        The page you are looking for might have been removed had its name
+        changed or is temporarily unavailable.
+      </p>
+      <NavLink to={"/"}>
+        <button className="button button--big">home</button>
+      </NavLink>
     </div>
   );
 };
